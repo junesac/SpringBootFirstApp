1392f9544e385310836f5fe9c9f921153bb7fe2d
Sachin Kumar <sjun1992@gmail.com>
2018-04-27 15:46:32 +0530

    Added logged in user information in ActivityController

--- src/main/java/com/controller/ActivityController.java
+++ src/main/java/com/controller/ActivityController.java
@@ -1,23 +1,25 @@
 package com.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.model.UserActivity;
 
 @RestController
 @RequestMapping("activity")
 public class ActivityController {
 
-  @RequestMapping(value = "get", method = RequestMethod.GET, produces = {"application/json"})
-  public UserActivity getActivity() {
-    UserActivity activity = new UserActivity("Running", 20L);
-    return activity;
-  }
+	@RequestMapping(value = "get", method = RequestMethod.GET, produces = { "application/json" })
+	public UserActivity getActivity() {
+		UserActivity activity = new UserActivity("Running", 20L);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String name = auth.getName();
 
-  @RequestMapping(value = "get", method = RequestMethod.POST, produces = {"application/json"})
-  public UserActivity getActivityPost() {
-    UserActivity activity = new UserActivity("Running", 20L);
-    return activity;
-  }
+		System.out.println("Logged in user name : " + name);
+		System.out.println(auth.getAuthorities());
+		return activity;
+	}
 }
